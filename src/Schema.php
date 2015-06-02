f3bcce520dddac17e82475989d0d8d82842ed071
romeOz <serggalka@gmail.com>
2015-06-02 07:21:05 +0300

    [skip CI] Fixed grammar.

--- src/Schema.php
+++ src/Schema.php
@@ -28,7 +28,7 @@ abstract class Schema implements ObjectInterface
 {
     use ObjectTrait;
     /**
-     * The followings are the supported abstract column data types.
+     * The following are the supported abstract column data types.
      */
     const TYPE_PK = 'pk';
     const TYPE_BIGPK = 'bigpk';
